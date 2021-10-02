7b7a8d6cd4adf40916617455f196f3d7e151748b
neil <akunamatata285@gmail.com>
2021-10-02 18:25:06 +0300

    lesson 2 is complete

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let title,
-    screens,
-    screePrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = "BigProject",
+    screens = "Simple, Hard, Interactive",
+    screenPrice = 2000,
+    rollback = 20,
+    fullPrice = 20000,
+    adaptive = true;
 
-alert('old flashbacks');
+
+    //lesson1
+//alert('old flashbacks');
 
 console.log('any text');
+
+
+//lesson2
+for (let i of [title, fullPrice, adaptive]) console.log(typeof i);
+
+console.log(screens.length);
+
+console.log(`the price of 1 screen is ${screenPrice} \n the full price is ${fullPrice}`);
+
+screens = screens.toLowerCase().split();
+console.log(screens);
+
+console.log(`broker's payment is ${fullPrice * (rollback/100)}`);
