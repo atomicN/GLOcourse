3ac65ad1e61fbbed3e9774303134b8740fe18150
neil <akunamatata285@gmail.com>
2021-10-05 21:37:07 +0300

    extra task is complete

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-
-// 1
-const lang = prompt('choose your language','"ru" or "en"').toLowerCase();
-const ruWeak = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
-const enWeak = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
-
-if (lang == "ru") log(ruWeak);
-if (lang == "en") log(enWeak);
-
-
-switch (lang){
-    case "ru":
-        log(ruWeak);
-        break;
-    case "en":
-        log(enWeak);
-        break;
-}
-
-let langArray = [];
-
-langArray['ru'] = ruWeak;
-langArray['en'] = enWeak;
-log(langArray[lang]);
-
-
-
-
-
-
-//2
-
-const namePerson = prompt('Ваше имя?');
-
-namePerson.toLowerCase() == "артем" ? log("директор") :
-            namePerson == "александр" ? log("преподаватель"): log("студент");
-
-
-
-function log(elem){
-    console.log(elem);
-}
+const stringCheck = string => {
+    if (typeof string !== "string") {
+        console.log('invalid input : not a string');
+        return;
+    }
+    string.trim();
+    if (string.length > 30) return string.slice(0,30) + '...';
+    return string;
+};
