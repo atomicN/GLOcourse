f3248b8f0675c9da1a0e8b75289a2119e883a2f4
neil <akunamatata285@gmail.com>
2021-10-05 21:15:29 +0300

    lesson 4 is complete

--- script.js
+++ script.js
@@ -1,49 +1,61 @@
 "use strict"
 
 
-let title = prompt('what`s your project`s name?'),
+let title = prompt('what`s your project`s name?').trim(),
     screens = prompt('what kind of screens do you want?',`"simple","hard","interactive"`),
-    screenPrice = prompt("how much will this work cost?"),
-    rollback = 20,
+    screenPrice = +prompt("how much will this work cost?","please enter only numbers"),
+    adaptive = confirm("do you need an adaptive site?"),
+    service = prompt("what kind of extra service would you like?"),
+    service2 = prompt("what kind of second extra service would you like?");
+let servicePrice,
+    servicePrice2,
     fullPrice,
-    adaptive = confirm("do you need an adaptive site?");
+    allServicePrices,
+    servicePercentPrice,
+    rollback = 20;
 
-let service = prompt("what kind of extra service would you like?"),
-    servicePrice;
-if (service) servicePrice = +prompt("how much would you like to pay for it?","please enter only numbers");
 
-let    service2 = prompt("what kind of extra service would you like?"),
-    servicePrice2;
-if (service2) servicePrice2 = +prompt("how much would you like to pay for it?","please enter only numbers");
+function getServicePrice(serv){
+    if (serv) {
+        return +prompt(`how much would you like to pay for ${serv}?`,"please enter only numbers");
+    }
+}
 
-fullPrice = +screenPrice + servicePrice + servicePrice2;
+const showTypeOf = variable => console.log(`${variable}: `, typeof variable);
 
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+const getAllServicePrices = (serv1, serv2) => serv1 + serv2;
 
-log(servicePercentPrice);
+function getFullPrice(){
+    return screenPrice + allServicePrices;
+}
 
-switch (fullPrice){
-    case fullPrice > 30000 || fullPrice == 30000:
-        log("you got a 10% discount");
-        break;
-    case fullPrice > 15000 && fullPrice < 30000 || fullPrice == 15000:
-        log("you got a 5% discount");
-        break;
-    case fullPrice > 0 && fullPrice < 15000:
-        log("a discount is not provided");
-        break;
-    case fullPrice < 0 :
-        log("something went wrong");
+function getTitle(name) {
+    return name[0].toUpperCase() + name.slice(1,).toLowerCase();
+}
+
+function getServicePercentPrices() {
+    return Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+}
+
+function getRollBackMessage (price){
+    if (price > 30000 || price == 30000) console.log("you got a 10% discount");
+    if (price > 15000 && price < 30000 || price == 15000) console.log("you got a 5% discount");
+    if (price > 0 && price < 15000) console.log("a discount is not provided");
+    if (price < 0)  console.log("something went wrong");
 }
 
 
+title = getTitle(title);
+servicePrice = getServicePrice(service);
+servicePrice2 = getServicePrice(service2);
+allServicePrices = getAllServicePrices(servicePrice, servicePrice2);
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+getRollBackMessage(fullPrice);
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
 
 
-
-
-
-
-
-function log(elem) {
-    console.log(elem);
-}
+screens.trim().includes(',') ? console.log(screens.trim().split(',')) : console.log(screens.trim().split(' '));
+console.log(servicePercentPrice);
