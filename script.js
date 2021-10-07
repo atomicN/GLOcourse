1ef0d3f79514ff869ffe3559580f7cba03ba02db
neil <akunamatata285@gmail.com>
2021-10-08 02:14:27 +0300

    lesson 5 2-nd extra task is complete

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const stringCheck = string => {
-    if (typeof string !== "string") {
-        console.log('invalid input : not a string');
-        return;
+// 1
+// const arr = ['234','123','321','421','341','412','214'];
+// for (let i in arr){
+//     if (arr[i].startsWith('2') || arr[i].startsWith('4')){
+//         console.log(arr[i]);
+//     }
+// }
+
+// 2
+for (let i = 2; i <= 100; i++) {
+    let flag = 0;
+
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
     }
-    string.trim();
-    if (string.length > 30) return string.slice(0,30) + '...';
-    return string;
-};
+
+    if (i > 1 && flag == 0) {
+        console.log(`${i} divisors - 1:${i}`);
+    }
+}
