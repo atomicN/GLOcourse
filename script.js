488cf29e942bd49cfcc7ad89b395dde541c7dcce
neil <akunamatata285@gmail.com>
2021-10-08 01:14:39 +0300

    lesson 5 is complete

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
 "use strict"
 
 
-let title = prompt('what`s your project`s name?').trim(),
-    screens = prompt('what kind of screens do you want?',`"simple","hard","interactive"`),
-    screenPrice = +prompt("how much will this work cost?","please enter only numbers"),
-    adaptive = confirm("do you need an adaptive site?"),
-    service = prompt("what kind of extra service would you like?"),
-    service2 = prompt("what kind of second extra service would you like?");
-let servicePrice,
-    servicePrice2,
+let title ,
+    screens,
+    screenPrice,
+    adaptive,
+    service,
+    service2,
     fullPrice,
     allServicePrices,
     servicePercentPrice,
     rollback = 20;
 
 
-function getServicePrice(serv){
-    if (serv) {
-        return +prompt(`how much would you like to pay for ${serv}?`,"please enter only numbers");
+const isNumber = num =>!isNaN(parseFloat(num)) && isFinite(num);
+
+const questionnaire = () => {
+    title = prompt('what`s your project`s name?').trim();
+    screens = prompt('what kind of screens do you want?',`"simple","hard","interactive"`);
+
+    do {screenPrice = prompt("how much would you like to pay for it?","please enter only numbers");}
+    while(!isNumber(screenPrice));
+
+    adaptive = confirm("do you need an adaptive site?");
+};
+
+const getAllServicePrices = () => {
+    let total = 0;
+    let servicePrice;
+    for (let i = 0; i < 2; i++){
+        if (i === 0)  service = prompt('what kind of extra service do you want?');
+        else if(i === 1) service2 = prompt('what kind of extra service do you want?');
+        do {servicePrice = prompt('how much will it cost?');}
+        while (!isNumber(servicePrice));
+        total += +servicePrice;
+        servicePrice = 0;
     }
-}
+    return total;
+};
 
-const showTypeOf = variable => console.log(`${variable}: `, typeof variable);
+const getFullPrice = () => +screenPrice + allServicePrices;
 
-const getAllServicePrices = (serv1, serv2) => serv1 + serv2;
+const getServicePercentPrices = () => Math.ceil(fullPrice - (fullPrice * (rollback/100)));
 
-function getFullPrice(){
-    return screenPrice + allServicePrices;
-}
-
-function getTitle(name) {
-    return name[0].toUpperCase() + name.slice(1,).toLowerCase();
-}
-
-function getServicePercentPrices() {
-    return Math.ceil(fullPrice - (fullPrice * (rollback/100)));
-}
-
-function getRollBackMessage (price){
+const getRollBackMessage = price =>{
     if (price > 30000 || price == 30000) console.log("you got a 10% discount");
     if (price > 15000 && price < 30000 || price == 15000) console.log("you got a 5% discount");
     if (price > 0 && price < 15000) console.log("a discount is not provided");
     if (price < 0)  console.log("something went wrong");
-}
+};
+
+const getTitle = name => name[0].toUpperCase() + name.slice(1,).toLowerCase();
+
+const showTypeOf = variable => console.log(`${variable}: `, typeof variable);
 
 
+
+questionnaire();
 title = getTitle(title);
-servicePrice = getServicePrice(service);
-servicePrice2 = getServicePrice(service2);
-allServicePrices = getAllServicePrices(servicePrice, servicePrice2);
+allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 servicePercentPrice = getServicePercentPrices();
 getRollBackMessage(fullPrice);
