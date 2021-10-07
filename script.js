36f598840b2ae85c3b9a9b655584606c0dfa9bb1
neil <akunamatata285@gmail.com>
2021-10-08 02:16:36 +0300

    uncommented

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // 1
-// const arr = ['234','123','321','421','341','412','214'];
-// for (let i in arr){
-//     if (arr[i].startsWith('2') || arr[i].startsWith('4')){
-//         console.log(arr[i]);
-//     }
-// }
+const arr = ['234','123','321','421','341','412','214'];
+for (let i in arr){
+    if (arr[i].startsWith('2') || arr[i].startsWith('4')){
+        console.log(arr[i]);
+    }
+}
 
 // 2
 for (let i = 2; i <= 100; i++) {
