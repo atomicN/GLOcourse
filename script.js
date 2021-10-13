b390f3a07118abdade1c314aef4369516c6e6d0c
neil <akunamatata285@gmail.com>
2021-10-14 00:05:40 +0300

    lesson8 is complete

--- script.js
+++ script.js
@@ -3,66 +3,80 @@
 
 const appData = {
     title: '',
-    screens: '',
+    screens: [],
     screenPrice: 0,
     adaptive: true,
-    service: '',
-    service2: '',
+    services: {},
     fullPrice: 0,
     allServicePrices: 0,
     servicePercentPrice: 0,
     rollback: 20,
     questionnaire: () => {
-        appData.title = prompt('what`s your project`s name?').trim();
-        appData.screens = prompt('what kind of screens do you want?',`"simple","hard","interactive"`);
-    
-        do {appData.screenPrice = prompt("how much would you like to pay for it?","please enter only numbers");}
-        while(!isNumber(appData.screenPrice));
+        do {appData.title = prompt('what`s your project`s name?').trim();}
+        while(isNumber(appData.title) || appData.title.length == 0);
+
+        for (let i = 0; i < 2; i++){
+            let name;
+            do {name = prompt('what kind of screens do you want?').trim();}
+            while(isNumber(name) || name.length == 0);
+            let price = 0;
+            
+            do {price = prompt("how much would you like to pay for it?","please enter only numbers");}
+            while(!isNumber(price));
+
+            appData.screens.push({id: i, name: name, price: price});
+        }
+
+        for (let i = 0; i < 2; i++){
+            let name;
+            do {name = prompt('what kind of extra service do u want?').trim();}
+            while(isNumber(name) || name.length == 0);
+            let servicePrice = 0;
+            
+            do {servicePrice = prompt('how much will it cost?');}
+            while (!isNumber(servicePrice));
+
+            appData.services[name + (i + 1)] = +servicePrice;
+        }
     
         appData.adaptive = confirm("do you need an adaptive site?");
     },
-    getAllServicePrices: () => {
-        let total = 0;
-        let servicePrice;
-        for (let i = 0; i < 2; i++){
-            if (i === 0)  appData.service = prompt('what kind of extra service do you want?');
-            else if(i === 1) appData.service2 = prompt('what kind of extra service do you want?');
-            do {servicePrice = prompt('how much will it cost?');}
-            while (!isNumber(servicePrice));
-            total += +servicePrice;
-            servicePrice = 0;
-        }
-        return total;
+    concatPrices: () => {
+        for (let screen of appData.screens) { appData.screenPrice += +screen.price; }
+        for (let key in appData.services) appData.allServicePrices += +appData.services[key];
     },
-    getFullPrice: () => +appData.screenPrice + appData.allServicePrices,
-    getServicePercentPrices: () => Math.ceil(appData.fullPrice - (appData.fullPrice * (appData.rollback/100))),
-    logger: function (variable){ console.log(`${variable}: `, typeof variable); },
+    getFullPrice: () => appData.fullPrice = +appData.screenPrice + appData.allServicePrices,
+    getServicePercentPrices: () => appData.servicePercentPrice = Math.ceil(appData.fullPrice - (appData.fullPrice * (appData.rollback/100))),
+    logger: () => {
+        console.log(appData.title);
+        console.log(appData.screenPrice);
+        console.log(appData.adaptive);
+        console.log(appData.servicePercentPrice);
+        console.log(appData.screens);
+        console.log(appData.services);
+    },
     start: () => {
         appData.questionnaire();
-        appData.title = capitalizeTitle(appData.title);
-        appData.allServicePrices = appData.getAllServicePrices();
-        appData.fullPrice = appData.getFullPrice();
-        appData.servicePercentPrice = appData.getServicePercentPrices();
-        getRollBackMessage(appData.fullPrice);
-        appData.logger(appData.title);
-        appData.logger(appData.screenPrice);
-        appData.logger(appData.adaptive);
-        appData.logger(appData.servicePercentPrice);
-        for (let key in appData) appData.logger(key);
-    }
+        appData.capitalizeTitle();
+        appData.concatPrices();
+        appData.getFullPrice();
+        appData.getServicePercentPrices();
+        appData.getRollBackMessage(appData.fullPrice);
+        appData.logger();
+    },
+    getRollBackMessage: price =>{
+        if (price > 30000 || price == 30000) console.log("you got a 10% discount");
+        if (price > 15000 && price < 30000 || price == 15000) console.log("you got a 5% discount");
+        if (price > 0 && price < 15000) console.log("a discount is not provided");
+        if (price < 0)  console.log("something went wrong");
+    },
+    capitalizeTitle: () => appData.title = appData.title[0].toUpperCase() + appData.title.slice(1,).toLowerCase()
 };
 
 
 function isNumber(num){ return !isNaN(parseFloat(num)) && isFinite(num); }
 
-function capitalizeTitle(name){ return name[0].toUpperCase() + name.slice(1,).toLowerCase(); }
 
-const getRollBackMessage = price =>{
-    if (price > 30000 || price == 30000) console.log("you got a 10% discount");
-    if (price > 15000 && price < 30000 || price == 15000) console.log("you got a 5% discount");
-    if (price > 0 && price < 15000) console.log("a discount is not provided");
-    if (price < 0)  console.log("something went wrong");
-};
 
 
 appData.start();
