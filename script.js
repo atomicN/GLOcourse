a3ac67efa8b64efc3e18c1239dbaca58af7b45c0
neil <akunamatata285@gmail.com>
2021-10-25 16:47:58 +0300

    level16 is complete

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
-let elem;
-const DomElement = function(selector, height, width, bg, fontSize, pos){
-    this.selector = selector;
-    this.height = height; 
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.pos = pos;
-};
-DomElement.prototype.create = function(){
-    if (this.selector.startsWith('.'))  elem = document.createElement('div');  
-    if (this.selector.startsWith('#'))  elem = document.createElement('p');
-    elem.style.cssText =`   height: ${this.height}px;
-                            width: ${this.width}px;
-                            background-color: ${this.bg};
-                            font-size: ${this.fontSize};
-                            position: ${this.pos};
-                        `;
-};
 
+class First{
+    hello(){
+        console.log('привет, я метод родителя');
+    }
+}
 
-const newElem = new DomElement('.elem', 100, 500, 'red', 20, 'absolute');
-newElem.create();
-document.addEventListener('DOMContentLoaded', () => document.body.append(elem));
+class Second extends First{
+    hello(){
+        super.hello();
+        console.log('а я дочерний метод родителя');
+    }
 
-document.body.addEventListener("keydown", function (event) {
-    if (event.code != "ArrowRight" && event.code != "ArrowLeft" &&
-        event.code != "ArrowUp" && event.code != "ArrowDown") return;
+}
 
-    let rectElem = elem.getBoundingClientRect();
-    let x = rectElem.x + pageXOffset,
-        y = rectElem.y + pageYOffset;
-
-    if (event.code == "ArrowRight") x += 10 ;
-    if (event.code == "ArrowLeft")  x -= 10 ;
-    if (event.code == "ArrowUp")    y -= 10 ;
-    if (event.code == "ArrowDown")  y += 10 ;
-    
-    elem.style.position = "absolute";
-    elem.style.left = x + "px";
-    elem.style.top = y + "px";
-});
+const obj = new Second();
+obj.hello();
